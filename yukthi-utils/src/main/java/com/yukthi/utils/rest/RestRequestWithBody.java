907c0b943b7f9ce4a30521b7f0a55bdfd289691e
A Kranthi Kiran <akranthikiran@gmail.com>
2015-11-21 07:49:12 +0530

    Added support for files (yet to be tested)

--- yukthi-utils/src/main/java/com/yukthi/utils/rest/RestRequestWithBody.java
+++ yukthi-utils/src/main/java/com/yukthi/utils/rest/RestRequestWithBody.java
@@ -6,7 +6,7 @@ package com.yukthi.utils.rest;
 import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.IdentityHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +28,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Represents Rest request method with body
+ * 
  * @author akiran
  */
 public abstract class RestRequestWithBody<T extends RestRequestWithBody<T>> extends RestRequest<T>
@@ -36,22 +37,28 @@ public abstract class RestRequestWithBody<T extends RestRequestWithBody<T>> exte
 	 * Request body
 	 */
 	private String requestBody;
-	
+
 	/**
-	 * Map to hold file fields. Identity hash map is used to support multiple files for same field.
+	 * Map to hold file fields.
 	 */
-	private Map<String, File> fileParams = new IdentityHashMap<String, File>();
-	
+	private Map<String, FileInfo> fileAttachments = new HashMap<>();
+
 	/**
 	 * Object mapper that will be used to convert objects to json
 	 */
 	private ObjectMapper objectMapper = new ObjectMapper();
-	
+
 	/**
 	 * HTTP method represented by this request
 	 */
 	private String method;
-	
+
+	/**
+	 * Name of the request body field used, when multipart request is built
+	 * (when request has file attachments)
+	 */
+	private String requestFieldName = "request";
+
 	/**
 	 * @param uri
 	 */
@@ -60,28 +67,50 @@ public abstract class RestRequestWithBody<T extends RestRequestWithBody<T>> exte
 		super(uri);
 		this.method = method;
 	}
-	
+
 	/**
-	 * Sets the request body.
-	 * Note: params and request body can not be used on same request
+	 * Gets the name of the request body field used, when multipart request is built (when request has file attachments).
+	 *
+	 * @return the name of the request body field used, when multipart request is built (when request has file attachments)
+	 */
+	public String getRequestFieldName()
+	{
+		return requestFieldName;
+	}
+
+	/**
+	 * Sets the name of the request body field used, when multipart request is built (when request has file attachments).
+	 *
+	 * @param requestFieldName the new name of the request body field used, when multipart request is built (when request has file attachments)
+	 */
+	public void setRequestFieldName(String requestFieldName)
+	{
+		this.requestFieldName = requestFieldName;
+	}
+
+	/**
+	 * Sets the request body. Note: params and request body can not be used on
+	 * same request
+	 * 
 	 * @param body
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
 	public T setBody(String body)
 	{
-		if(!super.params.isEmpty() || !this.fileParams.isEmpty())
+		if(!super.params.isEmpty() || !this.fileAttachments.isEmpty())
 		{
 			throw new IllegalStateException("Both params and body can not be set on a single request. Param(s) were already set");
 		}
-		
+
 		this.requestBody = body;
-		return (T)this;
+		return (T) this;
 	}
 
 	/**
-	 * Converts specified object into json and sets it as request body.
-	 * Note: Params and request body can not be used on same request
+	 * Converts specified object into json and sets it as request body. Note:
+	 * Params and request body can not be used on same request
+	 * 
 	 * @param object
 	 * @return
 	 */
@@ -90,14 +119,17 @@ public abstract class RestRequestWithBody<T extends RestRequestWithBody<T>> exte
 		try
 		{
 			return setBody(objectMapper.writeValueAsString(object));
-		}catch(JsonProcessingException ex)
+		} catch(JsonProcessingException ex)
 		{
 			throw new IllegalArgumentException("Failed to format specified object as json - " + object, ex);
 		}
 	}
-	
-	/* (non-Javadoc)
-	 * @see com.yodlee.common.rest.RestRequest#addParam(java.lang.String, java.lang.String)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.yodlee.common.rest.RestRequest#addParam(java.lang.String,
+	 * java.lang.String)
 	 */
 	@Override
 	public T addParam(String name, String value)
@@ -106,89 +138,110 @@ public abstract class RestRequestWithBody<T extends RestRequestWithBody<T>> exte
 		{
 			throw new IllegalStateException("Both params and body can not be set on a single request. Request body was already set");
 		}
-		
+
 		return super.addParam(name, value);
 	}
-	
+
 	/**
 	 * Adds file param to the request
-	 * @param name Name of the field
-	 * @param file File to be attached
+	 * 
+	 * @param field
+	 *            Name of the field
+	 * @param file
+	 *            File to be attached
+	 * @param contentType File content mime type
 	 */
-	public void addFileParam(String name, File file)
+	public void addAttachment(String field, File file, String contentType)
 	{
 		if(requestBody != null)
 		{
 			throw new IllegalStateException("Both params and body can not be set on a single request. Request body was already set");
 		}
-		
-		this.fileParams.put(new String(name), file);
+
+		this.fileAttachments.put(field, new FileInfo(file, contentType));
 	}
 
-	/* (non-Javadoc)
-	 * @see com.yodlee.common.rest.RestRequest#toHttpRequestBase(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.yodlee.common.rest.RestRequest#toHttpRequestBase(java.lang.String)
 	 */
 	@Override
 	public HttpRequestBase toHttpRequestBase(String baseUrl) throws UnsupportedEncodingException
 	{
 		HttpEntityEnclosingRequestBase postRequest = newRequest(baseUrl);
-		
-		super.populateHeaders(postRequest);
-		
-		//set the content type if specified
-		if(super.getContentType() != null)
-		{
-			postRequest.setHeader(HttpHeaders.CONTENT_TYPE, super.getContentType());
-		}
 
-		//if file params are attached
-		if(!this.fileParams.isEmpty())
+		super.populateHeaders(postRequest);
+
+		// if file params are attached
+		if(!this.fileAttachments.isEmpty())
 		{
 			MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 			builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
-			
-			//add files to request body
-			for(String name : this.fileParams.keySet())
+
+			FileInfo fileInfo = null;
+
+			// add files to request body
+			for(String name : this.fileAttachments.keySet())
 			{
-				builder.addPart(name, new FileBody(this.fileParams.get(name)));
+				fileInfo = fileAttachments.get(name);
+
+				if(fileInfo.getContentType() != null)
+				{
+					builder.addPart(name, new FileBody(fileInfo.getFile(), ContentType.create(fileInfo.getContentType())));
+				}
+				else
+				{
+					builder.addPart(name, new FileBody(fileInfo.getFile()));
+				}
 			}
-			
-			//add params to request bodu
-			for(String name : super.params.keySet())
+
+			if(requestBody != null)
 			{
-				builder.addPart(name, new StringBody(super.params.get(name), ContentType.MULTIPART_FORM_DATA));
+				builder.addPart(requestFieldName, new StringBody( requestBody, ContentType.create(super.getContentType()) ) );
 			}
 		}
-		//if params are present set the request body in the http FORM format
+		// if params are present set the request body in the http FORM format
 		else if(!super.params.isEmpty())
 		{
 			List<NameValuePair> urlParameters = new ArrayList<NameValuePair>(super.params.size());
-			
+
 			for(String paramName : super.params.keySet())
 			{
 				urlParameters.add(new BasicNameValuePair(paramName, super.params.get(paramName)));
 			}
-			
+
 			postRequest.setEntity(new UrlEncodedFormEntity(urlParameters));
 		}
-		//if no params are present but request body is present, set body as simple string
+		// if no params are present but request body is present, set body as
+		// simple string
 		else if(requestBody != null)
 		{
+			// set the content type if specified
+			if(super.getContentType() != null)
+			{
+				postRequest.setHeader(HttpHeaders.CONTENT_TYPE, super.getContentType());
+			}
+
 			postRequest.setEntity(new StringEntity(requestBody));
 		}
-		
+
 		return postRequest;
 	}
-	
+
 	/**
-	 * Factory method. Child classes are expected to override this method and provide new request object.
+	 * Factory method. Child classes are expected to override this method and
+	 * provide new request object.
+	 * 
 	 * @param baseUrl
 	 * @return
 	 */
 	protected abstract HttpEntityEnclosingRequestBase newRequest(String baseUrl);
-	
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
@@ -200,12 +253,12 @@ public abstract class RestRequestWithBody<T extends RestRequestWithBody<T>> exte
 		builder.append("\n\tRequest Type: ").append(method);
 		builder.append("\n\t").append("Resolved URI: ").append(super.getResolvedUri());
 		builder.append("\n\t").append("Params: ").append(super.params);
-		
+
 		if(!headers.isEmpty())
 		{
 			builder.append("\n\t").append("Headers: ").append(super.headers);
 		}
-		
+
 		builder.append("\n\t").append("Body: ").append(requestBody);
 
 		builder.append("\n]");
