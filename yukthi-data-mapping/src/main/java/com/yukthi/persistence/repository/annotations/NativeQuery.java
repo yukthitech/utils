78d86d6c71acb18475a25837778b5593f802d900
yukthitech <akranthi@yukthi-tech.co.in>
2016-01-27 19:32:12 +0530

    Started with Native query support

---
+++ yukthi-data-mapping/src/main/java/com/yukthi/persistence/repository/annotations/NativeQuery.java
@@ -0,0 +1,31 @@
+package com.yukthi.persistence.repository.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to mark target function as native query executor. Native queries should be avoided to the extent possible so
+ * that the application is target db independent.
+ * 
+ * Native queries are added by using 
+ * 
+ * @author akiran
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+public @interface NativeQuery
+{
+	/**
+	 * Specifies of the name of the native query to execute
+	 * @return
+	 */
+	public String name();
+	
+	/**
+	 * Specifies the type of the native query. Default type: READ
+	 * @return
+	 */
+	public NativeQueryType type() default NativeQueryType.READ;
+}
