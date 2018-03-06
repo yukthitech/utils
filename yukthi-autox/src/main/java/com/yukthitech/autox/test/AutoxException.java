1a5aab4d593c06878581f11933947b6fd7e53570
A Kranthi Kiran <akranthi@yukthi-tech.co.in>
2018-03-06 21:02:30 +0530

    Adding missing file

---
+++ yukthi-autox/src/main/java/com/yukthitech/autox/test/AutoxException.java
@@ -0,0 +1,37 @@
+package com.yukthitech.autox.test;
+
+import com.yukthitech.autox.IStep;
+import com.yukthitech.utils.exceptions.UtilsException;
+
+public class AutoxException extends UtilsException
+{
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Step which is throwing this exception.
+	 */
+	private IStep sourceStep;
+
+	/**
+	 * Instantiates a new autox exception.
+	 *
+	 * @param sourceStep the source step
+	 * @param message the message
+	 * @param args the args
+	 */
+	public AutoxException(IStep sourceStep, String message, Object... args)
+	{
+		super(message, args);
+		this.sourceStep = sourceStep;
+	}
+	
+	/**
+	 * Gets the step which is throwing this exception.
+	 *
+	 * @return the step which is throwing this exception
+	 */
+	public IStep getSourceStep()
+	{
+		return sourceStep;
+	}
+}
