0caa1fbbd29c199670d585ddfe5c63675cc90f05
A Kranthi Kiran <akranthi@yukthi-tech.co.in>
2019-05-30 10:28:01 +0530

    Added serialization based persistence using converter

---
+++ yukthi-data/src/main/java/com/yukthitech/persistence/conversion/impl/SerializationConverter.java
@@ -0,0 +1,88 @@
+package com.yukthitech.persistence.conversion.impl;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import java.sql.Blob;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.SerializationUtils;
+
+import com.yukthitech.persistence.annotations.DataType;
+import com.yukthitech.persistence.annotations.DataTypeMapping;
+import com.yukthitech.persistence.conversion.IPersistenceConverter;
+import com.yukthitech.utils.exceptions.InvalidStateException;
+
+/**
+ * This is not a default converter, if needed, this needs to be used explicitly on target entity fields
+ * using {@link DataTypeMapping}. 
+ * 
+ * This converters converts any object into byte[] using serialization. And the converted bytep[ is expected to be persisted into db (in BLOB) columns.
+ * 
+ * @author akiran
+ */
+public class SerializationConverter implements IPersistenceConverter
+{
+	/* (non-Javadoc)
+	 * @see com.fw.persistence.conversion.IPersistenceConverter#convertToJavaType(java.lang.Object, com.fw.persistence.annotations.DataType, java.lang.Class)
+	 */
+	@Override
+	public Object convertToJavaType(Object dbObject, DataType dbType, Class<?> javaType)
+	{
+		if(!(dbObject instanceof byte[]))
+		{
+			dbObject = toBytes(dbObject);
+		}
+		
+		try
+		{
+			return SerializationUtils.deserialize((byte[]) dbObject);
+		}catch(Exception ex)
+		{
+			throw new InvalidStateException("An error occurred while parsing serialization data", ex);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see com.fw.persistence.conversion.IPersistenceConverter#convertToDBType(java.lang.Object, com.fw.persistence.annotations.DataType)
+	 */
+	@Override
+	public Object convertToDBType(Object javaObject, DataType dbType)
+	{
+		try
+		{
+			return SerializationUtils.serialize((Serializable) javaObject);
+		}catch(Exception ex)
+		{
+			throw new InvalidStateException("An error occurred while serializing input data: {}", javaObject, ex);
+		}
+	}
+
+	/**
+	 * Converts specified db object into string
+	 * @param dbObj
+	 * @return
+	 */
+	private static byte[] toBytes(Object dbObj)
+	{
+		//if db object is string
+		//if specified type is blob
+		if(dbObj instanceof Blob)
+		{
+			Blob blob = (Blob)dbObj;
+			
+			try
+			{
+				InputStream is = blob.getBinaryStream();
+				byte[] res = IOUtils.toByteArray(is);
+				
+				is.close();
+				return res;
+			}catch(Exception ex)
+			{
+				throw new IllegalStateException("An error occured while reading blob data.", ex);
+			}
+		}
+
+		throw new IllegalStateException(String.format("Unsupported db data type %s encountered for Serialization conversion", dbObj.getClass().getName()));
+	}
+}
