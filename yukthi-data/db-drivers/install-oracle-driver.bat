f6807cf1dbc951627b5f72c78dfc469cfe067127
A Kranthi Kiran <akranthi@yukthtech.com>
2019-10-19 16:37:38 +0530

    oracle driver related changes

---
+++ yukthi-data/db-drivers/install-oracle-driver.bat
@@ -0,0 +1,6 @@
+cd %~dp0
+set JAVA_HOME=C:\Java\jdk1.8.0_92
+
+C:\Java\apache-maven-3.2.5\bin\mvn install:install-file -Dfile=./ojdbc8-12.2.0.1.jar -DgroupId=com.oracle.jdbc -DartifactId=ojdbc8 -Dversion=12.2.0.1 -Dpackaging=jar
+
+pause
