9e938088c9fb30800613922cb930f8a323e0fc53
akranthikiran <akranthikiran@akranthi>
2021-07-26 22:02:08 +0530

    Correct the report font

--- yukthi-autox/src/test/java/com/yukthitech/test/TAutomation.java
+++ yukthi-autox/src/test/java/com/yukthitech/test/TAutomation.java
@@ -26,7 +26,7 @@ public class TAutomation
 				"-rf", "./output", 
 				"-prop", "./src/test/resources/app.properties", 
 				//"-ts", "rest-test-suites"
-				"-tc", "testExpressions"
+				"-tc", "assertEquals"
 				//"-list", "com.yukthitech.autox.event.DemoModeAutomationListener"
 			});
 		
