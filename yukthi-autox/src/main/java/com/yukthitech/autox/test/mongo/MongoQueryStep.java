af166e5aca685c12a98bbfdbcb72641c6e796ead
akranthikiran <akranthikiran@akranthi>
2022-08-04 23:09:17 +0530

    Mongo bug fix

--- yukthi-autox/src/main/java/com/yukthitech/autox/test/mongo/MongoQueryStep.java
+++ yukthi-autox/src/main/java/com/yukthitech/autox/test/mongo/MongoQueryStep.java
@@ -9,7 +9,6 @@ import com.yukthitech.autox.ExecutionLogger;
 import com.yukthitech.autox.Group;
 import com.yukthitech.autox.Param;
 import com.yukthitech.autox.SourceType;
-import com.yukthitech.utils.exceptions.InvalidStateException;
 
 /**
  * Executes specified mongo Query on specified mongo resource defined in https://docs.mongodb.com/manual/reference/command.
@@ -62,10 +61,5 @@ public class MongoQueryStep extends AbstractStep
 		
 		exeLogger.debug("Using attribute '{}' setting the obtained result. Result: {}", resultAttribute, result);
 		context.setAttribute(resultAttribute, result);
-		
-		if(!"1.0".equals("" + result.get("ok")))
-		{
-			throw new InvalidStateException("Query execution failed as the result does not contain entry: {ok: 1.0}");
-		}
 	}
 }
