182962e02ed25563c8104e7ce734e476691a1840
Kranthi <Kranthi@DESKTOP-4LL1317>
2023-06-06 12:34:19 +0530

    Adding ability to specify custom validator

---
+++ yukthi-validators/src/main/java/com/yukthitech/validators/SelfValidator.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2022 "Yukthi Techsoft Pvt. Ltd." (http://yukthitech.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yukthitech.validators;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.ValidationException;
+
+import com.yukthitech.validation.ISelfValidation;
+import com.yukthitech.validation.annotations.EnableSelfValidation;
+
+public class SelfValidator implements ConstraintValidator<EnableSelfValidation, Object>
+{
+	@Override
+	public void initialize(EnableSelfValidation constraintAnnotation)
+	{
+	}
+
+	@Override
+	public boolean isValid(Object value, ConstraintValidatorContext context)
+	{
+		if(!(value instanceof ISelfValidation))
+		{
+			return true;
+		}
+		
+		try
+		{
+			((ISelfValidation) value).validate();
+		}catch(ValidationException ex)
+		{
+			context.buildConstraintViolationWithTemplate(ex.getMessage()).addConstraintViolation();
+			return false;
+		}
+		
+		return true;
+	}
+}
